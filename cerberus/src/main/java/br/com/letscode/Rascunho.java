bdc7eed7a029d85520a48e8817e8f585e48f42b1
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-05 14:10:14 -0300

    Tirando rascunho do git

--- cerberus/src/main/java/br/com/letscode/Rascunho.java
+++ cerberus/src/main/java/br/com/letscode/Rascunho.java
@@ -5,7 +5,5 @@ import br.com.letscode.modelos.Pessoa;
 
 public class Rascunho {
 	public static void main(String[] args) {
-		Pessoa joao = new Pessoa("João", 37, 'M', 'C', "Lojista");
-		System.out.println(joao);
 	}
 }
