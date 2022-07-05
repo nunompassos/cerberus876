d052830451517fea385a0ae55519687cdb86fbd9
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-05 14:04:17 -0300

    Conta abstrata - primeira versão

---
+++ cerberus/src/main/java/br/com/letscode/excecoes/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package br.com.letscode.excecoes;
+
+public class SaldoInsuficienteException extends RuntimeException{
+	public SaldoInsuficienteException() {
+		super();
+	}
+}
---
+++ cerberus/src/main/java/br/com/letscode/modelos/Conta.java
@@ -0,0 +1,76 @@
+package br.com.letscode.modelos;
+
+import br.com.letscode.excecoes.SaldoInsuficienteException;
+
+public abstract class Conta {
+	protected final int numero;
+	protected Pessoa titular;
+	protected long saldo;
+	protected long limite;
+
+	Conta(int numero, Pessoa titular) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = 0;
+		this.limite = 0;
+	}
+
+	public void depositar(long deposito) {
+		this.saldo += deposito;
+	}
+
+	public long sacar(long saque) {
+		if (saque < this.saldo) {
+			this.saldo -= saque;
+			return saque;
+		} else {
+			throw new SaldoInsuficienteException();
+		}
+	}
+
+	public void transferir(long transferencia, Conta destino) {
+		destino.depositar(this.sacar(transferencia));
+	}
+
+	public long getSaldoPuro() {
+		return saldo;
+	}
+
+	public String getSaldoFormatado() {
+		String digitos = String.valueOf(this.getSaldoPuro());
+		String reais = digitos.substring(0, digitos.length() - 2);
+		String centavos = digitos.substring(digitos.length() - 2, digitos.length());
+		return "R$ " + reais + ',' + centavos;
+	}
+
+	public Pessoa getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Pessoa titular) {
+		this.titular = titular;
+	}
+
+	public long getLimite() {
+		return limite;
+	}
+
+	public void setLimite(long limite) {
+		this.limite = limite;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta: {" +
+		this.getNumero() + " " +
+		this.getSaldoFormatado() + " " +
+		this.getTitular() + " " +
+		'}';
+		
+	}
+
+}
