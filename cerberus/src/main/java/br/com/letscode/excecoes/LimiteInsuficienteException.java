02d387bba447cf6e3a4ca031048b2e5562d132f1
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-05 15:40:00 -0300

    Mais exceções

---
+++ cerberus/src/main/java/br/com/letscode/excecoes/LimiteInsuficienteException.java
@@ -0,0 +1,7 @@
+package br.com.letscode.excecoes;
+
+public class LimiteInsuficienteException extends RuntimeException{
+	public LimiteInsuficienteException(){
+		super();
+	}
+}
---
+++ cerberus/src/main/java/br/com/letscode/excecoes/PagamentoExcessivoException.java
@@ -0,0 +1,7 @@
+package br.com.letscode.excecoes;
+
+public class PagamentoExcessivoException extends RuntimeException{
+	public PagamentoExcessivoException() {
+		super();
+	}
+}
