52f00b04de10f7fab50e6993ceede1cf3013ac68
F9329134 Thiago Teixeira de Faria <thiago.teixeira.bb.com.br>
2022-07-07 18:45:24 -0300

    Altera nome de classe Taxavel.java

--- cerberus/src/main/java/br/com/letscode/Models/ContaCorrentePJ.java
+++ cerberus/src/main/java/br/com/letscode/Models/ContaCorrentePJ.java
@@ -3,12 +3,13 @@ package br.com.letscode.models;
 import java.math.BigDecimal;
 
 import br.com.letscode.Utils.Utils;
-import br.com.letscode.interfaces.Tributavel;
+import br.com.letscode.interfaces.Taxavel;
 
-public class ContaCorrentePJ extends ContaCorrente implements Tributavel {
+public class ContaCorrentePJ extends ContaCorrente implements Taxavel {
   @Override
   public void sacar(BigDecimal quantidade) {
-    BigDecimal valorASacar = this.getSaldo().add(this.getSaldo().multiply(Tributavel.tributo));
+    //trocar pq é em cima do valor do saque/transferencia e nao do saldo.
+    BigDecimal valorASacar = this.getSaldo().add(this.getSaldo().multiply(Taxavel.tributo));
     boolean saldoIsSuficiente = Utils.saldoIsSuficiente(valorASacar, this.getSaldo());
     if (!saldoIsSuficiente) {
       System.out.println("SaldoInsuficiente");
