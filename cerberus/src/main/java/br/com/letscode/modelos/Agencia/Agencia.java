5acbd1b8764c1a67004c9009003d807fa5b4147a
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-07 16:57:48 -0300

    Agencia pronta

--- cerberus/src/main/java/br/com/letscode/modelos/Agencia/Agencia.java
+++ cerberus/src/main/java/br/com/letscode/modelos/Agencia/Agencia.java
@@ -1,44 +1,153 @@
 package br.com.letscode.modelos.Agencia;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
+import javax.lang.model.util.ElementScanner6;
+
+import br.com.letscode.enums.PerfilInvestidor;
+import br.com.letscode.excecoes.AgenciaDuplicadaException;
+import br.com.letscode.excecoes.ContaJaExisteException;
 import br.com.letscode.modelos.Conta.*;
 import br.com.letscode.modelos.Pessoa.*;
+import br.com.letscode.util.DigitoVerificador;
 
 public class Agencia {
-	private ArrayList<Conta> contas;
-	private ArrayList<ContaCorrente> contasCorrente;
-	private ArrayList<ContaPoupanca> contasPoupanca;
-	private ArrayList<ContaInvestimento> contasInvestimento;
 
-	private ArrayList<Pessoa> clientes;
-	private ArrayList<PessoaFisica> clientesPf;
-	private ArrayList<PessoaJuridica> clientesPj;
-	
-	public void addCliente(Pessoa pessoa) {
+	private final int numero;
+	private final static ArrayList<Integer> numerosDeAgencia = new ArrayList<Integer>();
+
+	private final ArrayList<Conta> contas;
+	private final ArrayList<Integer> numerosDeConta;
+
+	private final ArrayList<Pessoa> clientes;
+	private final ArrayList<PessoaFisica> clientesPf;
+	private final ArrayList<PessoaJuridica> clientesPj;
+
+	// [0] = CC, [1] = CI, [2] = CP
+	private final HashMap<Pessoa, Conta[]> mapPessoasContas;
+	private final HashMap<Class<?>, Integer> posicao = new HashMap<Class<?>, Integer>();
+	{
+		posicao.put(ContaCorrente.class, 0);
+		posicao.put(ContaInvestimento.class, 1);
+		posicao.put(ContaPoupanca.class, 2);
+	}
+
+	public Agencia(int numero) {
+		if (Agencia.numerosDeAgencia.contains(numero))
+			throw new AgenciaDuplicadaException();
+		this.numero = numero;
+		Agencia.numerosDeAgencia.add(numero);
+
+		mapPessoasContas = new HashMap<Pessoa, Conta[]>();
+
+		contas = new ArrayList<Conta>();
+		numerosDeConta = new ArrayList<Integer>();
+
+		clientes = new ArrayList<Pessoa>();
+		clientesPf = new ArrayList<PessoaFisica>();
+		clientesPj = new ArrayList<PessoaJuridica>();
+	}
+
+	public void cadastrarCliente(Pessoa cliente) {
+		if (cliente == null)
+			throw new IllegalArgumentException("Cliente não pode ser nulo");
+		if (PessoaFisica.class.isInstance(cliente))
+			this.cadastrarCliente((PessoaFisica) cliente);
+		else if (PessoaJuridica.class.isInstance(cliente))
+			this.cadastrarCliente((PessoaJuridica) cliente);
+		
+		this.abrirConta(cliente, ContaCorrente.class);
+	}
+
+	private void cadastrarCliente(PessoaFisica pessoa) {
 		if (clientes.contains(pessoa))
 			return;
 		clientes.add(pessoa);
+		clientesPf.add(pessoa);
+		mapPessoasContas.put(pessoa, new Conta[3]);
 	}
 
-	public Pessoa getCliente(int documento) {
-		for (Pessoa cliente : clientes) {
-			if (cliente.getDocumento() == documento)
-				return cliente;
-		}
-		return null;
-	}
-
-	public void addConta(Conta conta) {
-		if (contas.contains(conta))
+	private void cadastrarCliente(PessoaJuridica empresa) {
+		if (clientes.contains(empresa))
 			return;
-		contas.add(conta);
+		clientes.add(empresa);
+		clientesPj.add(empresa);
+		mapPessoasContas.put(empresa, new Conta[3]);
+
+		PessoaFisica dono = empresa.getResponsavel();
+		if (clientesPf.contains(dono))
+			return;
+		clientesPf.add(dono);
+	}
+
+	public void abrirConta(Pessoa cliente, Class<?> tipoDeConta) {
+		if (cliente == null)
+			throw new IllegalArgumentException("Cliente não pode ser nulo");
+		if (posicao.containsKey(tipoDeConta))
+			throw new IllegalArgumentException("tipoDeConta inválido");
+		verificaSeTemConta(cliente, tipoDeConta);
+
+		int numero = geraNumeroConta(tipoDeConta);
+		this.cadastrarCliente(cliente);
+
+		Conta novaConta;
+		if (tipoDeConta == ContaCorrente.class)
+			novaConta = new ContaCorrente(numero, cliente);
+		else if (tipoDeConta == ContaInvestimento.class)
+			novaConta = new ContaInvestimento(numero, cliente);
+		else if (tipoDeConta == ContaPoupanca.class)
+			novaConta = new ContaPoupanca(numero, (PessoaFisica) cliente);
+		else
+			throw new IllegalArgumentException("tipoDeConta inválido");
+
+		mapPessoasContas.get(cliente)[posicao.get(tipoDeConta)] = novaConta;
+		contas.add(novaConta);
+	}
+
+	private void verificaSeTemConta(Pessoa cliente, Class<?> tipoDeConta) {
+		if (mapPessoasContas.get(cliente)[posicao.get(tipoDeConta)] != null) {
+			Conta existente = mapPessoasContas.get(cliente)[0];
+			throw new ContaJaExisteException("Cliente já tem conta-corrente com número " + existente.getNumero());
+		}
+	}
+
+	private int geraNumeroConta(Class<?> tipoDeConta) {
+		int numero;
+		Random r = new Random();
+		int min = (int) Math.pow(10, DigitoVerificador.numeroDeDigitos);
+		int max = min * 10 - 1;
+
+		// Colocando o primeiro numero, que corresponde ao tipo de conta
+		min = Integer.parseInt(posicao.get(tipoDeConta) + 1 + String.valueOf(min));
+		max = Integer.parseInt(posicao.get(tipoDeConta) + 1 + String.valueOf(max));
+
+		do {
+			int numeroSemDigito = min + (int) r.nextDouble() * (max - min);
+			int digito = DigitoVerificador.calculaDigito(numeroSemDigito);
+			numero = numeroSemDigito * 10 + digito;
+		} while (numerosDeConta.contains(numero));
+		numerosDeConta.add(numero);
+		return numero;
+	}
+
+	public int getNumero() {
+		return this.numero;
 	}
 
 	public Conta getConta(int numero) {
 		for (Conta conta : contas) {
 			if (conta.getNumero() == numero)
 				return conta;
+		}
+		return null;
+	}
+
+	public Pessoa getCliente(int documento) {
+		for (Pessoa cliente : clientes) {
+			if (cliente.getDocumento() == documento)
+				return cliente;
 		}
 		return null;
 	}
