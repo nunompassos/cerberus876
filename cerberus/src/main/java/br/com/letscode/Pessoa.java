8eb37aa80298629145d61a99c5fe4191e024cefa
F8236816 Pedro Faustino de Oliveira Rodrigues de Melo <pedro.faustino@bb.com.br>
2022-07-14 13:32:27 -0300

    Arruma projeto no vscode

--- cerberus/src/main/java/br/com/letscode/Pessoa.java
+++ cerberus/src/main/java/br/com/letscode/Pessoa.java
@@ -3,7 +3,7 @@ package br.com.letscode;
 import java.io.Serializable;
 import java.security.InvalidParameterException;
 
-public class Pessoa implements Cloneable, Serializable {
+public class Pessoa implements Cloneable, Serializable, Comparable<Pessoa>{
 
     private String nome;
     private String apelido;
@@ -64,5 +64,9 @@ public class Pessoa implements Cloneable, Serializable {
     public String toString() {
         return "Eu chamo-me " + this.nome + " " + this.apelido + " e tenho " + this.idade + " anos."
             + (this.cpf != 0 ? " O meu CPF é: " + this.cpf : "");
+    }
+    @Override
+    public int compareTo(Pessoa o) {
+        return this.getIdade() - o.getIdade();
     }
 }
