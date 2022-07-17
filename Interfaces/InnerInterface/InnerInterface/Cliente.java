cfa228ab6f0ad20102d2f570e3cf62bff54f397f
Ana Dib <ana_dib@yahoo.com>
2022-07-16 23:16:17 -0300

    innerinterfaces

---
+++ Interfaces/InnerInterface/InnerInterface/Cliente.java
@@ -0,0 +1,23 @@
+package InnerInterface;
+
+import java.math.BigDecimal;
+
+public class Cliente {    
+    private BigDecimal saldo;  
+
+    public void setSaldo(BigDecimal saldo) {
+       this.saldo = saldo;
+    }
+
+    public BigDecimal getSaldo() {
+        return this.saldo;
+     }
+    
+    public interface OperacaoSaldoAtualizado{
+        public BigDecimal saldoAtualizado();
+    }
+}
+
+
+
+
---
+++ Interfaces/InnerInterface/InnerInterface/TestInnerInterface.java
@@ -0,0 +1,14 @@
+package InnerInterface;
+import java.math.BigDecimal;
+
+public class TestInnerInterface {
+    private static BigDecimal bonus = new BigDecimal("100.00");
+    private static BigDecimal saldo = new BigDecimal("200.00");
+    public static void main(String[] args) {
+
+      ClienteEspecial cl = new ClienteEspecial(bonus);
+      cl.setSaldo(saldo);
+      cl.saldoAtualizado();
+      
+  }
+}
