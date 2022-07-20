40022b56ee60f9e4a66a4a4e7bc17379146c6641
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-20 13:16:53 -0300

    Organizando app

--- cerberus/src/main/java/br/com/letscode/App.java
+++ cerberus/src/main/java/br/com/letscode/App.java
@@ -1,5 +1,11 @@
 package br.com.letscode;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import br.com.letscode.excecoes.BancoJayException;
 import br.com.letscode.modelos.Agencia;
 import br.com.letscode.modelos.Banco;
@@ -7,15 +13,9 @@ import br.com.letscode.modelos.conta.ContaCorrente;
 import br.com.letscode.modelos.pessoa.Pessoa;
 import br.com.letscode.modelos.pessoa.PessoaFisica;
 import br.com.letscode.telas.TelaInicial;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.FileNotFoundException;
 
 public class App {
-    public static void main(String[] args) throws BancoJayException, IOException {
+    public static void main(String[] args) throws BancoJayException, IOException, ClassNotFoundException {
 
         Agencia ag1 = new Agencia(1);
         Pessoa ze = new PessoaFisica(
@@ -30,12 +30,9 @@ public class App {
         Banco.agencias.add(ag1);
         Banco.selecionada = ag1;
         
-        // try {
-        //     ObjectInputStream ois = new ObjectInputStream(new FileInputStream("dados.bin"));
-        //     Banco.selecionada = (Agencia) ois.readObject();
-        //     ois.close();
-        // } catch (ClassNotFoundException | FileNotFoundException e) {
-        // }
+        // ObjectInputStream ois = new ObjectInputStream(new FileInputStream("dados.bin"));
+        // Banco.selecionada = (Agencia) ois.readObject();
+        // ois.close();
         
         new TelaInicial();
 
