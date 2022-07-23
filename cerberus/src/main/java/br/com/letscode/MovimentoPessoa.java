b2da53099a267d63f7710d19367ff15a50c4ee2d
nuno.passos <nuno.passos@ibanwallet.com>
2022-07-23 11:45:21 +0100

    Resolver erros

--- cerberus/src/main/java/br/com/letscode/MovimentoPessoa.java
+++ cerberus/src/main/java/br/com/letscode/MovimentoPessoa.java
@@ -3,11 +3,11 @@ package br.com.letscode;
 public class MovimentoPessoa {
 
     public void andarPe(Pessoa p, int distancia) {
-        System.out.println("A pessoa " + p.getNome() + " andou " + distancia + " metros!");
+        System.out.println("A pessoa " + p.getNomeCompleto() + " andou " + distancia + " metros!");
     }
 
     public void andarCarro(Pessoa p, int distancia) {
-        System.out.printf("A pessoa %s ligou o carro e andou %d kms!", p.getNome(), distancia);
+        System.out.printf("A pessoa %s ligou o carro e andou %d kms!", p.getNomeCompleto(), distancia);
     }
 
 }
