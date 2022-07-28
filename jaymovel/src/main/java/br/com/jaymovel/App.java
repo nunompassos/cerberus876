ab6d635d7de967370c2faa9d6c3f09ac145c149b
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-28 14:39:59 -0300

    Mais atributos no Veiculo

--- jaymovel/src/main/java/br/com/jaymovel/App.java
+++ jaymovel/src/main/java/br/com/jaymovel/App.java
@@ -10,54 +10,68 @@ import br.com.jaymovel.modelos.veiculo.Veiculo;
 import br.com.jaymovel.telas.TelaInicial;
 
 public class App {
-    public static void main(String[] args) throws JayMovelException {
+	public static void main(String[] args) throws JayMovelException {
 
-        Agencia a = new Agencia();
+		Agencia a = new Agencia();
 
-        Pessoa ze = new PessoaFisica(
-                "Zé da Silva",
-                123,
-                "Rua da Goiaba, 420",
-                "(66) 6666-6666",
-                "2000-01-01");
+		Pessoa ze = new PessoaFisica(
+				"Zé da Silva",
+				123,
+				"Rua da Goiaba, 420",
+				"(66) 6666-6666",
+				"2000-01-01");
+		a.adicionaCliente(ze);
 
-        Pessoa sandubas = new PessoaJuridica(
-                "Sandubas",
-                456,
-                "endereco",
-                "telefone",
-                (PessoaFisica) ze);
-        a.adicionaCliente(ze);
-        a.adicionaCliente(sandubas);
-        
-        Veiculo carroPequeno = new Veiculo(TipoVeiculo.PEQUENO, 1);
-        Veiculo carroMedio = new Veiculo(TipoVeiculo.MEDIO, 2);
-        Veiculo suv = new Veiculo(TipoVeiculo.SUV, 3);
-        a.adicionaVeiculo(carroPequeno);
-        a.adicionaVeiculo(carroMedio);
-        a.adicionaVeiculo(suv);
-        // TODO criar um acervo de carros para pôr na Agência aqui na App
+		a.adicionaCliente(new PessoaJuridica(
+				"Sandubas",
+				456,
+				"endereco",
+				"telefone",
+				(PessoaFisica) ze));
 
-        // a.adicionaCliente(ze);
-        // CadastroCliente cadastroZe = a.getCadastro(123);
-        // cadastroZe.adicionaAluguel(carroMedio, 4);
-        // System.out.println(cadastroZe.getDivida());
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.PEQUENO,
+				1,
+				"Suziki",
+				"SRAD",
+				2021,
+				"Vermelha"));
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.PEQUENO,
+				2,
+				"Suziki",
+				"GSX-S750ZA",
+				2022,
+				"Azul"));
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.PEQUENO,
+				3,
+				"Cherry",
+				"QQ",
+				2019,
+				"Branco"));
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.MEDIO,
+				4,
+				"Renault",
+				"Clio",
+				2017,
+				"Prata"));
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.PEQUENO,
+				5,
+				"Chevrolet",
+				"Celta",
+				2012,
+				"Preto"));
+		a.adicionaVeiculo(new Veiculo(
+				TipoVeiculo.SUV,
+				6,
+				"Dodge",
+				"Ram",
+				2021,
+				"Bege"));
 
-        // a.adicionaCliente(sandubas);
-        // CadastroCliente cadastroSandubas = a.getCadastro(456);
-        // cadastroSandubas.adicionaAluguel(carroMedio, 1);
-        // cadastroSandubas.adicionaAluguel(suv, 6);
-        // cadastroSandubas.adicionaAluguel(carroPequeno, 2);
-        // System.out.println(cadastroSandubas.getDivida());
-
-        // ObjectInputStream ois = new ObjectInputStream(new
-        // FileInputStream("dados.bin"));
-        // Banco.selecionada = (Agencia) ois.readObject();
-        // ois.close();
-        new TelaInicial(a);
-        // ObjectOutputStream oos = new ObjectOutputStream(new
-        // FileOutputStream("dados.bin"));
-        // oos.writeObject(Banco.selecionada);
-        // oos.close();
-    }
+		new TelaInicial(a);
+	}
 }
--- jaymovel/src/main/java/br/com/jaymovel/modelos/veiculo/Veiculo.java
+++ jaymovel/src/main/java/br/com/jaymovel/modelos/veiculo/Veiculo.java
@@ -5,13 +5,46 @@ import br.com.jaymovel.enums.TipoVeiculo;
 public class Veiculo {
 	private final TipoVeiculo tipo;
 	private final int chassi;
-	//TODO Colocar mais campos (modelo, cor etc)
+	private final String marca;
+	private final String modelo;
+	private final int ano;
+	private String cor;
 
-	public Veiculo(TipoVeiculo tipo, int chassi) {
+	public Veiculo(
+			TipoVeiculo tipo,
+			int chassi,
+			String marca,
+			String modelo,
+			int ano,
+			String cor) {
 		if (tipo == null)
 			throw new IllegalArgumentException("Tipo de veículo não pode ser nulo");
 		this.tipo = tipo;
 		this.chassi = chassi;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.cor = cor;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getMarca() {
+		return marca;
 	}
 
 	public int getChassi() {
@@ -25,8 +58,12 @@ public class Veiculo {
 	@Override
 	public String toString() {
 		return String.format(
-			"[Tipo: %s]",
-			tipo,
-			chassi);
+				"%s %s %s %s, Tipo: %s",
+				marca,
+				modelo,
+				ano,
+				cor,
+				tipo,
+				chassi);
 	}
 }
