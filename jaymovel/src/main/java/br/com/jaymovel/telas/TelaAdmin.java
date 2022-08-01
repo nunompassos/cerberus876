3c6ffc78c4f40927da68d974ccbfc15758826ad4
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-08-01 14:17:55 -0300

    Formatação TelaAdmin

--- jaymovel/src/main/java/br/com/jaymovel/telas/TelaAdmin.java
+++ jaymovel/src/main/java/br/com/jaymovel/telas/TelaAdmin.java
@@ -11,7 +11,7 @@ import br.com.jaymovel.modelos.veiculo.Veiculo;
 import br.com.jaymovel.util.Formulario;
 
 public class TelaAdmin extends Tela {
-    private static final Map<Integer, String> opcoes = new HashMap<>();
+	private static final Map<Integer, String> opcoes = new HashMap<>();
 	{
 		opcoes.put(1, "Cadastrar veículo");
 		opcoes.put(2, "Sair");
@@ -27,9 +27,9 @@ public class TelaAdmin extends Tela {
 
 	}
 
-    @Override
-    protected void opcao1() {
-        Veiculo veiculo = Formulario.cadastrarVeiculo();
+	@Override
+	protected void opcao1() {
+		Veiculo veiculo = Formulario.cadastrarVeiculo();
 		try {
 			Tela.agencia.adicionaVeiculo(veiculo);
 		} catch (VeiculoDuplicadoException e) {
@@ -37,9 +37,9 @@ public class TelaAdmin extends Tela {
 			return;
 		}
 		System.out.println("Veículo cadastrado com sucesso!");
-    }
+	}
 
-    @Override
+	@Override
 	protected void opcao2() throws SairDaTelaException {
 		throw new SairDaTelaException();
 	}
